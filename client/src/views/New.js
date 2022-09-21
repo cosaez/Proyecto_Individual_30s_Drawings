ce27a50a7487c389b02f95c8c35acc2509df49ed
cosaez <c.olivaresaez@gmail.com>
2022-09-21 20:59:46 -0300

    v1.4

--- client/src/views/New.js
+++ client/src/views/New.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useContext } from 'react';
 import { UserContext } from '../context/userContext';
+import toast, { Toaster } from 'react-hot-toast';
 const New = () => {
     const context = useContext(UserContext);
     const [image, setImage] = useState("");
@@ -14,30 +15,32 @@ const New = () => {
             "image": image
         })
             .then((res) => {
-                console.log(res)
+                console.log(res);
+                toast.success('Imagen añadida')
             })
             .catch((err) => {
                 console.log(err)
             })
         console.log(image);
-        alert('Imagen añadida');
     }
     return (
         <div>
             {
                 context.user && <Form onSubmit={handleSubmit}>
                     <FormGroup className="form-group">
-                        <Label>Image Url</Label>
+                        <Label>Url de imagen</Label>
                         <Input type="url" required value={image} onChange={(e) => setImage(e.target.value)} />
                     </FormGroup>
                     <Button>Añadir Imagen</Button>
                 </Form>
+                
             }
             {
                 !context.user && <div>
                     <p>Conectate para añadir más imágenes</p>
                 </div>
             }
+            <Toaster />
             <Link to={'/list'}>
                 <Button>
                     Volver
